2d7c189771fc12975e38ec4f2b138619a64811df
Khizhny <khizhny@gmail.com>
2017-03-15 13:19:05 +0200

    Widget Updates after new SMS bug fixed

--- app/src/main/java/com/khizhny/smsbanking/SmsReciever.java
+++ app/src/main/java/com/khizhny/smsbanking/SmsReciever.java
@@ -4,16 +4,19 @@ import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 
 import xml.SmsBankingWidget;
 
 public class SmsReciever extends  android.content.BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Bundle bundle = intent.getExtras();
+        //Bundle bundle = intent.getExtras();
+        /*}
+
         if (bundle != null)
-        {
+        {*/
+        if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            //update all widgets if new SMS recieve
             ComponentName name = new ComponentName(context, SmsBankingWidget.class);
             int[] ids = AppWidgetManager.getInstance(context).getAppWidgetIds(name);
             Intent update = new Intent();
@@ -23,3 +26,4 @@ public class SmsReciever extends  android.content.BroadcastReceiver {
         }
     }
 }
+
